59dd94186e4d0535e63c598dcb71dde88a3a5080
wps5422 <127423674+wps5422@users.noreply.github.com>
2023-03-16 00:02:27 +0700

    Update extern.hpp

--- extern.hpp
+++ extern.hpp
@@ -38,4 +38,46 @@ class Integer{
 
         return a;
     }
+  typedef void (*random_func)(uint8_t *bytes, size_t n_bytes);
+
+    static Integer random_bits(size_t n_bits, random_func func) {
+        if (n_bits == 0) return 0;
+        size_t partial_bits = n_bits % 64;
+        size_t n_words = n_bits / 64 + (partial_bits > 0);
+        size_t n_bytes = n_words * sizeof(word);
+        Integer result(n_words, 0);
+        auto *bytes = (uint8_t *) &result[0];
+        func(bytes, n_bytes);
+        if (partial_bits) {
+            size_t too_many_bits = 64 - partial_bits;
+            result.words.back() >>= too_many_bits;
+        }
+        return result;
+    }
+
+    static Integer random_inclusive(const Integer &inclusive, random_func func) {
+        size_t n_bits = inclusive.bit_size();
+        while (true) {
+            Integer result = random_bits(n_bits, func);
+            if (result <= inclusive) return result;
+        }
+    }
+
+    static Integer random_exclusive(const Integer &exclusive, random_func func) {
+        size_t n_bits = exclusive.bit_size();
+        while (true) {
+            Integer result = random_bits(n_bits, func);
+            if (result < exclusive) return result;
+        }
+    }
+
+    [[maybe_unused]] static Integer
+    random_second_exclusive(const Integer &inclusive_min_val, const Integer &exclusive_max_val, random_func func) {
+        return inclusive_min_val + random_exclusive(exclusive_max_val - inclusive_min_val, func);
+    }
+
+    [[maybe_unused]] static Integer
+    random_both_inclusive(const Integer &inclusive_min_val, const Integer &inclusive_max_val, random_func func) {
+        return inclusive_min_val + random_inclusive(inclusive_max_val - inclusive_min_val, func);
+    }
 }
