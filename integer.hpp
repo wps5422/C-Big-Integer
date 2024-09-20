f6c2a84109fea1b1a27ded32955a7d3d332b892b
Admin <cvb000x00@gmail.com>
2024-09-21 04:48:47 +0700

    add const

--- integer.hpp
+++ integer.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #ifdef _MSC_VER
 #include <sstream>
 #endif
@@ -20,12 +21,15 @@ public:
     bool is_negative = false;
     static constexpr bool SAFE_INITIALIZATION = false;
 
-    Integer() : is_negative(false) {};
+    Integer() : is_negative(false) {
+    };
 
-    Integer(size_t n, word w, bool negative = false) : words(n, w), is_negative(negative) {}
+    Integer(const size_t n, const word w, const bool negative = false) : words(n, w), is_negative(negative) {
+    }
 
-    [[maybe_unused]] Integer(const word *a, const word *b, bool negative = false) : words(a, b),
-                                                                                    is_negative(negative) {}
+    [[maybe_unused]] Integer(const word *a, const word *b, const bool negative = false) : words(a, b),
+        is_negative(negative) {
+    }
 
     Integer(const Integer &a) { words = a.words, is_negative = a.is_negative; }
 
@@ -72,13 +76,13 @@ public:
 
     [[maybe_unused]] explicit Integer(const char *s) : is_negative(false) { read((std::string) s); }
 
-    inline word &operator[](size_t i) { return words[i]; }
+    word &operator[](const size_t i) { return words[i]; }
 
-    const word &operator[](size_t i) const { return words[i]; }
+    const word &operator[](const size_t i) const { return words[i]; }
 
-    inline bool is_zero() const { return words.empty(); }
+    bool is_zero() const { return words.empty(); }
 
-    Integer &set_negative(bool b) {
+    Integer &set_negative(const bool b) {
         this->is_negative = b;
         return *this;
     }
@@ -90,22 +94,23 @@ public:
 
     size_t bit_size() const {
         if (words.empty()) return 0;
-        size_t last = words.size() - 1;
-        size_t result = word_bit_size(words[last]) + last * 64;
+        const size_t last = words.size() - 1;
+        const size_t result = word_bit_size(words[last]) + last * 64;
         return result;
     }
 
     static int cmp_abs(const Integer &a, const Integer &b) {
         if (a.words == b.words) return 0;
-        return std::lexicographical_compare(a.words.rbegin(), a.words.rend(), b.words.rbegin(), b.words.rend()) ? -1
-                                                                                                                : 1;
+        return std::lexicographical_compare(a.words.rbegin(), a.words.rend(), b.words.rbegin(), b.words.rend())
+                   ? -1
+                   : 1;
     }
 
-    static inline int cmp(const Integer &a, const Integer &b) {
+    static int cmp(const Integer &a, const Integer &b) {
         if (a.words.empty() && b.words.empty()) return 0;
-        else if (!a.is_negative && !b.is_negative) return +cmp_abs(a, b);
-        else if (a.is_negative && b.is_negative) return -cmp_abs(a, b);
-        else return a.is_negative && !b.is_negative ? -1 : +1;
+        if (!a.is_negative && !b.is_negative) return +cmp_abs(a, b);
+        if (a.is_negative && b.is_negative) return -cmp_abs(a, b);
+        return a.is_negative && !b.is_negative ? -1 : +1;
     }
 
     static size_t word_bit_size(word a) {
@@ -113,22 +118,28 @@ public:
         return 0;
     }
 
-    static word add_carry(word *a, word b) { return (*a += b) < b; }
+    static word add_carry(word *a, const word b) { return (*a += b) < b; }
 
     static word sub_carry(word *a, word b) {
-        word tmp = *a;
+        const word tmp = *a;
         return (*a = tmp - b) > tmp;
     }
 
-    static word word_mul_high(word a, word b) {
-        word a_high = a >> 32, a_low = a & UINT_MAX, b_high = b >> 32, b_low = b & UINT_MAX;
+    static word word_mul_high(const word a, const word b) {
+        const word a_high = a >> 32;
+        const word a_low = a & UINT_MAX;
+        const word b_low = b & UINT_MAX;
+        const word b_high = b >> 32;
         word tmp = ((a_low * b_low) >> 32) + a_high * b_low;
         tmp = (tmp >> 32) + ((a_low * b_high + (tmp & UINT_MAX)) >> 32);
         return tmp + a_high * b_high;
     }
 
     static Integer &add_unsigned_overwrite(Integer &a, const Integer &b) {
-        size_t i, na = a.words.size(), nb = b.words.size(), n = std::max(na, nb);
+        const size_t na = a.words.size();
+        const size_t nb = b.words.size();
+        size_t i;
+        const size_t n = std::max(na, nb);
         a.words.resize(n);
         word carry = 0;
         for (i = 0; i < nb; i++) {
@@ -141,7 +152,9 @@ public:
     }
 
     static Integer &sub_unsigned_overwrite(Integer &a, const Integer &b) {
-        size_t i, na = a.words.size(), nb = b.words.size();
+        size_t i;
+        const size_t nb = b.words.size();
+        const size_t na = a.words.size();
         word carry = 0;
         for (i = 0; i < nb; i++) {
             carry = sub_carry(&a[i], carry);
@@ -152,11 +165,14 @@ public:
     }
 
     static Integer native_multiple(const Integer &a, const Integer &b) {
-        size_t na = a.words.size(), nb = b.words.size(), nc = na + nb + 1;
+        const size_t na = a.words.size();
+        const size_t nb = b.words.size();
+        const size_t nc = na + nb + 1;
         Integer c(nc, 0, a.is_negative ^ b.is_negative), carries(nc, 0);
         for (size_t ia = 0; ia < na; ia++) {
             for (size_t ib = 0; ib < nb; ib++) {
-                size_t i = ia + ib, j = i + 1;
+                const size_t i = ia + ib;
+                const size_t j = i + 1;
                 carries[i + 1] += add_carry(&c[i], a[ia] * b[ib]);
                 carries[j + 1] += add_carry(&c[j], word_mul_high(a[ia], b[ib]));
             }
@@ -164,7 +180,7 @@ public:
         return add_unsigned_overwrite(c, carries).truncate();
     }
 
-    static void split(const Integer &a, Integer &lhs, Integer &rhs, size_t size_) {
+    static void split(const Integer &a, Integer &lhs, Integer &rhs, const size_t size_) {
         size_t i = 0;
         lhs.words.resize(size_);
         for (size_t j = 0; j < size_ && i < a.words.size(); j++) lhs[j] = a[i++];
@@ -175,14 +191,17 @@ public:
     }
 
     static Integer karatsuba_multiple(const Integer &a, const Integer &b) {
-        size_t na = a.words.size(), nb = b.words.size(), n = std::max(na, nb), m2 = n / 2 + (n & 1);
+        const size_t na = a.words.size();
+        const size_t nb = b.words.size();
+        const size_t n = std::max(na, nb);
+        size_t m2 = n / 2 + (n & 1);
         Integer a0, a1, b0, b1;
         split(a, a0, a1, m2);
         split(b, b0, b1, m2);
         m2 *= 64;
-        Integer z0 = a0 * b0;
-        Integer z1 = (a0 + a1) * (b0 + b1);
-        Integer z2 = a1 * b1;
+        const Integer z0 = a0 * b0;
+        const Integer z1 = (a0 + a1) * (b0 + b1);
+        const Integer z2 = a1 * b1;
         Integer result = z2;
         result <<= m2;
         result += z1 - z2 - z0;
@@ -194,8 +213,10 @@ public:
     static constexpr size_t threshold = 20;
 
     static Integer multiple(const Integer &a, const Integer &b) {
-        return (a.words.size() > threshold && b.words.size() > threshold ? karatsuba_multiple(a, b) : native_multiple(a,
-                                                                                                                      b));
+        return (a.words.size() > threshold && b.words.size() > threshold
+                    ? karatsuba_multiple(a, b)
+                    : native_multiple(a,
+                                      b));
     }
 
     static Integer add_signed(const Integer &a, bool a_negative, const Integer &b, bool b_negative) {
@@ -206,7 +227,7 @@ public:
 
     Integer &operator>>=(size_t n_bits) {
         if (n_bits == 0) return *this;
-        size_t n_words = n_bits / 64;
+        const size_t n_words = n_bits / 64;
         if (n_words >= words.size()) {
             words.resize(0);
             return *this;
@@ -217,9 +238,9 @@ public:
                 words[i] = words[i + n_words];
             }
         } else {
-            word high, low = words[n_words];
+            word low = words[n_words];
             for (size_t i = 0; i < words.size() - n_words - 1; i++) {
-                high = words[i + n_words + 1];
+                const word high = words[i + n_words + 1];
                 words[i] = (high << (64 - n_bits)) | (low >> n_bits);
                 low = high;
             }
@@ -231,17 +252,17 @@ public:
 
     Integer &operator<<=(size_t n_bits) {
         if (n_bits == 0) return *this;
-        size_t n_words = n_bits / 64;
+        const size_t n_words = n_bits / 64;
         n_bits %= 64;
-        size_t old_size = words.size();
-        size_t n = old_size + n_words + (n_bits != 0);
+        const size_t old_size = words.size();
+        const size_t n = old_size + n_words + (n_bits != 0);
         words.resize(n);
         if (n_bits == 0) {
             for (size_t i = n; i-- > n_words;) words[i] = words[i - n_words];
         } else {
-            word low, high = 0;
+            word high = 0;
             for (size_t i = n - 1; i > n_words; i--) {
-                low = words[i - n_words - 1];
+                const word low = words[i - n_words - 1];
                 words[i] = (high << n_bits) | (low >> (64 - n_bits));
                 high = low;
             }
@@ -255,7 +276,7 @@ public:
         quotient = 0;
         remainder = generator;
         if (cmp_abs(remainder, denominator) >= 0) {
-            int n = int(generator.bit_size() - denominator.bit_size());
+            int n = static_cast<int>(generator.bit_size() - denominator.bit_size());
             denominator <<= n;
             for (; n >= 0; n--) {
                 if (cmp_abs(remainder, denominator) >= 0) {
@@ -274,9 +295,9 @@ public:
         Integer dst(generator.words.size(), 0);
         for (size_t i = generator.words.size(); i-- > 0;) {
             word dst_word = 0;
-            word src_word = generator[i];
+            const word src_word = generator[i];
 
-            word part_0 = src_word >> 32, part_1 = src_word & UINT_MAX;
+            const word part_0 = src_word >> 32, part_1 = src_word & UINT_MAX;
             remainder <<= 32;
             remainder |= part_0;
             word div_word = remainder / BASE_DIGIT;
@@ -330,17 +351,18 @@ public:
         return result;
     }
 
-    Integer &set_bit(int i) {
-        size_t i_word = i / 64, i_bit = i % 64;
+    Integer &set_bit(const int i) {
+        const size_t i_word = i / 64;
+        const size_t i_bit = i % 64;
         if (words.size() <= i_word) words.resize(i_word + 1);
-        words[i_word] |= ((word) 1) << i_bit;
+        words[i_word] |= static_cast<word>(1) << i_bit;
         return *this;
     }
 
     void mul_word(word b) {
         word carry = 0;
         for (word &i: words) {
-            word a = i;
+            const word a = i;
             word tmp = a * b;
             carry = add_carry(&tmp, carry);
             carry += word_mul_high(a, b);
@@ -357,10 +379,10 @@ public:
         while (!tmp.words.empty()) {
             word remainder;
             div_mod_half_word(tmp, tmp, remainder);
-            result.push_back(char(MIN_NUMERIC_CHARACTER + remainder));
+            result.push_back(static_cast<char>(MIN_NUMERIC_CHARACTER + remainder));
         }
         if (is_negative) result.push_back('-');
-        std::reverse(result.begin(), result.end());
+        std::ranges::reverse(result);
         return result;
     }
 
@@ -433,6 +455,6 @@ public:
             is_negative = !is_negative;
             number *= -1;
         }
-        mul_word((word) number);
+        mul_word(static_cast<word>(number));
     }
 };
